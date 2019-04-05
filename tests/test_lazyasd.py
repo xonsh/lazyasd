c870c877bad3520d5b71020903a352e4a1d2c35e
Paul G <paulg@perforge.net>
2019-04-05 18:04:20 -0400

    Implement fine control of bg module load delay

--- tests/test_lazyasd.py
+++ tests/test_lazyasd.py
@@ -1,5 +1,7 @@
 """Tests lazy and self destruictive objects."""
-from lazyasd import LazyObject, load_module_in_background
+from lazyasd import LazyObject, load_module_in_background,\
+        BackgroundModuleDelayNoop, BackgroundModuleDelayExpBackoff,\
+        BackgroundModuleDelayConst, BackgroundModuleDelayEvent
 
 #
 # LazyObject Tests
@@ -12,5 +14,28 @@ def test_lazyobject_getitem():
 
 def test_bg_load():
     load_module_in_background('pkg_resources')
+    import pkg_resources
+    pkg_resources.iter_entry_points
+
+def test_bg_load_delay_noop():
+    load_module_in_background('pkg_resources', delayobj=BackgroundModuleDelayNoop())
+    import pkg_resources
+    pkg_resources.iter_entry_points
+
+def test_bg_load_delay_expbackoff():
+    load_module_in_background('pkg_resources', delayobj=BackgroundModuleDelayExpBackoff(5, 1, 2))
+    import pkg_resources
+    pkg_resources.iter_entry_points
+
+def test_bg_load_delay_const():
+    load_module_in_background('pkg_resources', delayobj=BackgroundModuleDelayConst(50))
+    import pkg_resources
+    pkg_resources.iter_entry_points
+
+def test_bg_load_delay_event():
+    delayobj=BackgroundModuleDelayEvent(50)
+    ev = delayobj.get_event()
+    load_module_in_background('pkg_resources', delayobj=delayobj)
+    ev.set()
     import pkg_resources
     pkg_resources.iter_entry_points
